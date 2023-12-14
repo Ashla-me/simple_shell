e6a555b33b09709a49bef7066d95edf58557ed9c
Precious Iwuanyanwu <iwuanyanwuprecious97@gmail.com>
2023-12-14 08:16:22 -0800

    Added stringDuplicate.c and stringCopy.c file

---
+++ stringCopy.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * _strcpy - Copy a string from source to destination.
+ * @destn: Destination buffer.
+ * @srce: Source string.
+ *
+ * Return: Pointer to destination buffer.
+ */
+char *_strcpy(char *destn, char *srce)
+{
+        int length = 0;
+        int i;
+
+        /* Calculate the length of the source string */
+        while (srce[length])
+                length++;
+
+        /* Copy characters from source to destination */
+        for (i = 0; i < length; i++)
+                destn[i] = srce[i];
+
+        /* Null-terminate the destination string */
+        destn[length] = '\0';
+
+        return (destn);
+}
---
+++ stringDuplicate.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * _strdup - Duplicate a string.
+ * @str: String to duplicate.
+ *
+ * Return: Pointer to the duplicated string or NULL if memory allocation fails.
+ */
+char *_strdup(char *str)
+{
+        char *duplicates;
+        size_t length;
+
+        length = _strlen(str) + 1;
+        duplicates = malloc(sizeof(char) * length);
+
+        if (duplicates != NULL)
+        {
+                _strcpy(duplicates, str);
+        }
+
+        return (duplicates);
+}
