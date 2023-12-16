f370ea1aa82e5b90e06d0c8cb9c7112431e9b0c5
Precious Iwuanyanwu <iwuanyanwprecious97@gmail.com>
2023-12-16 10:42:47 -0800

    Updated some files

--- shell.c
+++ shell.c
@@ -1,51 +1,44 @@
 #include "shell.h"
+
 /**
-* main - carries out the read, execute then print output loop
-* @argc: argument count
-* @argv: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
-
-int main(int argc, char **argv, char *envp[])
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	char *read = NULL, *pathcmd = NULL, *path = NULL;
-	size_t sip_size = 0;
-	ssize_t readsize = 0;
-	char **cmd = NULL, **pathway = NULL;
-	(void)envp, (void)argv;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	if (argc < 1)
-		return (-1);
-	signal(SIGINT, _signal);
-	while (1)
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
 	{
-		case_buffer(cmd);
-		case_buffer(pathway);
-		free(pathcmd);
-		unveil_prompt();
-		readsize = getline(&read, &sip_size, stdin);
-		if (readsize < 0)
-			break;
-		info.ln_count++;
-		if (read[readsize - 1] == '\n')
-			read[readsize - 1] = '\0';
-		cmd = token(read);
-		if (cmd == NULL || *cmd == NULL || **cmd == '\0')
-			continue;
-		if (look_check(cmd, read))
-			continue;
-		path = check_path();
-		pathway = token(path);
-		pathcmd = try_path(pathway, cmd[0]);
-		if (!pathcmd)
-			perror(argv[0]);
-		else
-			execute_cmmd(pathcmd, cmd);
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	if (readsize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(read);
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
