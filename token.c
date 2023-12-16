4f587e022242754ff0deef87586faf0a65f09489
Precious Iwuanyanwu <iwuanyanwprecious97@gmail.com>
2023-12-16 09:24:40 -0800

    Updated token.c file

--- token.c
+++ token.c
@@ -1,48 +1,95 @@
 #include "shell.h"
 
 /**
-* token - creates tokens from given input
-* @zip: to be tokenized
-*
-* Return: array of strings
-*/
-char **token(char *zip)
+ * **stringtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **stringtow(char *str, char *d)
 {
-	char *put = NULL, *read = NULL, *token = NULL, delim[] = {"\n "};
-	char **tokenize = NULL;
-	int red = 1;
-	size_t blue = 0, flag = 0;
+	int i, j, k, m, numwords = 0;
+	char **s;
 
-	put = _strdup(zip);
-	if (!put)
+	if (str == NULL || str[0] == 0)
 		return (NULL);
-	read = put;
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
 
-	while (*read)
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		if (_strchr(delim, *read) != NULL && flag == 0)
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
 		{
-			red++;
-			flag = 1;
-		}
-		else if (_strchr(delim, *read) == NULL && flag == 1)
-			flag = 0;
-		read++;
-	}
-	tokenize = malloc(sizeof(char *) * (red + 1));
-	token = strtok(put, delim);
-	while (token)
-	{
-		tokenize[blue] = _strdup(token);
-		if (tokenize[blue] == NULL)
-		{
-			free(tokenize);
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
 			return (NULL);
 		}
-		token = strtok(NULL, delim);
-		blue++;
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-	tokenize[blue] = '\0';
-	free(put);
-	return (tokenize);
+	s[j] = NULL;
+	return (s);
 }
+
+/**
+ * **stringtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **stringtow2(char *str, char d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+				    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
+
